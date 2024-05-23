56ac19a65605eb6457959071a57accf12fd9932b
Ubaid ur Rahman <ubaidurr888999@gmail.com>
2024-05-23 23:49:47 +0300

    added auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://noted-bobcat-27.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
